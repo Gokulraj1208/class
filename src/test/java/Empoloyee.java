ec6ed2ef8d76ec1c9a48760783f0eebaa92e6dc9
Gokulraj1208 <gokulrajaalambadi@gmail.com>
2022-11-07 12:52:06 +0530

    methods

--- src/test/java/Empoloyee.java
+++ src/test/java/Empoloyee.java
@@ -1,4 +1,13 @@
 
 public class Empoloyee {
+	public void A1() {
+		System.out.println("A");
+	}
+	public void A2() {
+		System.out.println("B");
+	}
+	public void A3() {
+		System.out.println("C");
+	}
 
 }
